7c13402d5b5ae898d33824606a743bf2306006a0
manager <manager@naver.com>
2026-05-04 17:23:30 +0900

    push by animal 01

---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("고양이 클래스");
+        System.out.println("야옹이 클래스");
+        System.out.println("살징이 클래스");
+        System.out.println("살징이 : 고양이를 귀엽게 부르는 경상도식 애칭");
+    }
+}
+
